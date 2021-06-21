f33e7c957ff37884a48574459564eabdc2f77648
Cristian Plesca <cplesca@zaizi.com>
2021-06-21 16:37:15 +0100

    We implement the 4 calls to the people service leaving off the delete for now

---
+++ src/main/java/com/example/peopleservicefacade/boundary/PersonFacade.java
@@ -0,0 +1,44 @@
+package com.example.peopleservicefacade.boundary;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+import com.example.peopleservicefacade.entity.Person;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "v1/people")
+public class PersonFacade {
+
+    @Autowired
+    PersonServiceClient personServiceClient;
+
+    @GetMapping
+    public List<Object> getPeople() {
+
+        return personServiceClient.getPeople();
+    }
+
+    @GetMapping("{id}")
+    public Object getPerson(@PathVariable("id") Long id) {
+
+        return personServiceClient.getPerson(id);
+    }
+
+    @PostMapping
+    public Object newPerson(@RequestBody @Valid Person person) {
+
+        return personServiceClient.newPerson(person);
+    }
+
+    @PutMapping("{id}")
+    public Object updatePerson(@PathVariable("id")Long id, @RequestBody Person person) {
+
+        return personServiceClient.updatePerson(id, person);
+    }
+}
