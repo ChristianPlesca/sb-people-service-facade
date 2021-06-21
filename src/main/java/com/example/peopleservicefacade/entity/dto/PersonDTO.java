cd9d15cb7d2763af529bcc744577d9243141bd65
Cristian Plesca <cplesca@zaizi.com>
2021-06-21 17:31:36 +0100

    Added AllArgsConstructor to Person DTO

--- src/main/java/com/example/peopleservicefacade/entity/dto/PersonDTO.java
+++ src/main/java/com/example/peopleservicefacade/entity/dto/PersonDTO.java
@@ -2,10 +2,12 @@ package com.example.peopleservicefacade.entity.dto;
 
 import com.example.peopleservicefacade.entity.Person;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 
 @Data
+@AllArgsConstructor
 public class PersonDTO {
 
     private Person person;
