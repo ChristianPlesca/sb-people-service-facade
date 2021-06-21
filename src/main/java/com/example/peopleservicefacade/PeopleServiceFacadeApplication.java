b540a4765d898d07c11dd82e81617d3b6a0e1e61
Cristian Plesca <cplesca@zaizi.com>
2021-06-21 15:47:53 +0100

    Initial Commit

---
+++ src/main/java/com/example/peopleservicefacade/PeopleServiceFacadeApplication.java
@@ -0,0 +1,13 @@
+package com.example.peopleservicefacade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleServiceFacadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleServiceFacadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/peopleservicefacade/PeopleServiceFacadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.peopleservicefacade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleServiceFacadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
